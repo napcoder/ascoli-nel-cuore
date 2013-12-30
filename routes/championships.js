7e21166eed587855d5b34d8f38ac9605fae4e816
Marco Travaglini <marcotravaglini81@gmail.com>
2013-12-30 18:03:13 +0100

    Added express and angular seed

---
+++ routes/championships.js
@@ -0,0 +1,35 @@
+var fs = require("fs");
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+  res.json(['1996-97', '1997-98', '1998-99']);
+};
+
+exports.year = function(req, res) {
+	var selYear = req.param('year');
+	console.log('selected year: ' + selYear);
+	if (selYear === '97') {
+		res.json({year: 1997});
+	} else if (selYear === '98') {
+		res.json({year: 1998});
+	} else {
+		res.json({year: 'unknowed'});
+	}
+};
+
+exports.example = function(req, res) {
+	handleChampionships(res, '/example');
+};
+
+
+var handleChampionships = function(response, pathname) {
+	console.log("Request handler " + pathname + " was called.");
+
+	fs.readFile('data_example.json', function(err, data) {
+		if (err) throw err;
+		response.json(JSON.parse(data));
+	});
+};
